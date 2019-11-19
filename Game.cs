dccab2591b7d079a48f96c2a30471fa934569d55
milomacphail <milomacphail@gmail.com>
2019-11-18 19:36:21 -0500

    Added gameplay engine.

--- Game.cs
+++ Game.cs
@@ -13,19 +13,32 @@ namespace HighLow
             int playerOneScore = 0;
             int playerTwoScore = 0;
 
-            if (cardOneValue > cardTwoValue)
+            Console.WriteLine("{0} of {1}", cardOne.CardValue, cardOne.CardSuit);
+            Console.WriteLine("Guess high or low: ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() != "high" || input.ToLower() != "low")
             {
+                Console.WriteLine("Please guess high or low: ");
+            }
+
+            if (cardOneValue > cardTwoValue && input == "high" || cardOneValue < cardTwoValue && input == "low")
+            {
+                Console.WriteLine("{0} of {1}", cardTwo.CardValue, cardTwo.CardSuit);
                 playerOneScore++;
                 Console.WriteLine("Player One wins with {0} point.", playerOneScore);
             }
-            else if (cardOneValue < cardTwoValue)
+            else if (cardOneValue < cardTwoValue && input == "high" || cardOneValue > cardTwoValue && input == "low")
             {
+                Console.WriteLine("{0} of {1}", cardTwo.CardValue, cardTwo.CardSuit);
                 playerTwoScore++;
                 Console.WriteLine("Player Two wins with {0} point.", playerTwoScore);
             }
             else
             {
-                Console.WriteLine("No winners here.");
+                Console.WriteLine("No winners here. Redrawing.");
+                Card newCard = new Card();
+                cardTwo = newCard;
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ namespace HighLow
         static void Main(string[] args)
         {
             Card cardOne = new Card();
-            Console.WriteLine("{0} of {1}", cardOne.CardValue, cardOne.CardSuit);
             Card cardTwo = new Card();
-            Console.WriteLine("{0} of {1}", cardTwo.CardValue, cardTwo.CardSuit);
             Game game = new Game(cardOne, cardTwo);
 
         }
